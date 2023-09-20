4c03104d3e479c2a86ce81080ed62b75db7aea9d
Vladimir Blinnikov <blinnik2001@yandex.ru>
2023-09-20 23:44:11 +0300

    feat: add Valid annotations for the PhoneController

--- src/main/java/com/sber/phonestore/phone/controller/PhoneController.java
+++ src/main/java/com/sber/phonestore/phone/controller/PhoneController.java
@@ -3,18 +3,22 @@ package com.sber.phonestore.phone.controller;
 import com.sber.phonestore.phone.model.Phone;
 import com.sber.phonestore.phone.service.PhoneService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/phones")
 @RequiredArgsConstructor
 public class PhoneController {
+
     private final PhoneService phoneService;
 
     @PostMapping
-    public Phone create(@RequestBody Phone phone) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public Phone create(@RequestBody @Valid Phone phone) {
         return phoneService.create(phone);
     }
 
@@ -29,11 +33,12 @@ public class PhoneController {
     }
 
     @PatchMapping("/{phoneId}")
-    public Phone update(@PathVariable Long phoneId, @RequestBody Phone phone) {
+    public Phone update(@PathVariable Long phoneId, @RequestBody @Valid Phone phone) {
         return phoneService.update(phoneId, phone);
     }
 
     @DeleteMapping("/{phoneId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long phoneId) {
        phoneService.delete(phoneId);
     }
