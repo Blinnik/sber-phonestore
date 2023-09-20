84333632b6b0bf08206dc2db24d28be02587d184
Vladimir Blinnikov <blinnik2001@yandex.ru>
2023-09-20 23:38:07 +0300

    feat: add a method that catches BadRequest errors

--- src/main/java/com/sber/phonestore/handler/ErrorHandler.java
+++ src/main/java/com/sber/phonestore/handler/ErrorHandler.java
@@ -3,16 +3,26 @@ package com.sber.phonestore.handler;
 import com.sber.phonestore.exception.NotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class ErrorHandler {
+
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundExceptions(final RuntimeException e) {
         return new ErrorResponse(e.getClass().getSimpleName(), e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class,
+            MethodArgumentTypeMismatchException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleBadRequestExceptions(final RuntimeException e) {
+        return new ErrorResponse(e.getClass().getSimpleName(), e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @AllArgsConstructor
